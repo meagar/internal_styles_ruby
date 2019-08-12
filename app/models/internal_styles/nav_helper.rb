e7f72c75b4a48f1fc21e8ed0c4499aaa1045611c
Matthew Eagar <meagar@gmail.com>
2019-08-12 10:54:25 -0400

    Add if: support for subnavs and nav items

--- app/models/internal_styles/nav_helper.rb
+++ app/models/internal_styles/nav_helper.rb
@@ -1,18 +1,26 @@
 module InternalStyles
   class NavHelper
-    attr_reader :items, :sub_navs
+    attr_reader :items, :sub_navs, :options
 
-    def initialize()
+    class Item < Struct.new(:label, :icon, :uri, :options) do
+        def active?(current_path)
+          current_path.starts_with?(uri)
+        end
+      end
+    end
+
+    def initialize(options = {})
       @items = []
       @sub_navs = {}
+      @options = options
     end
 
-    def item(name, icon, path)
-      items << [name, icon, path]
+    def item(name, icon, path, options = {})
+      items << Item.new(name, icon, path, options)
     end
 
-    def sub_nav(name)
-      sub_navs[name] ||= NavHelper.new
+    def sub_nav(name, options = {})
+      sub_navs[name] ||= NavHelper.new(options)
       yield sub_navs[name]
     end
   end
